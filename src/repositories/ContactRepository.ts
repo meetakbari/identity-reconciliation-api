f9a6c17389a9b80709e4f4cb121ff121ad16ecb1
meetakbari <meetakbari25@gmail.com>
2024-05-28 00:45:06 +0530

    Add: ContactResponse response types. getContact and getLinkedContacts repository methods.

--- src/repositories/ContactRepository.ts
+++ src/repositories/ContactRepository.ts
@@ -3,15 +3,33 @@ import { AppDataSource } from "../db/data-source";
 
 const contactRepository = AppDataSource.getRepository(Contact);
 
-const createContact = async (email: string, phoneNumber: string): Promise<Contact> => {
+const createContact = async (email: string, phoneNumber: string, linkedId: number, linkPrecedence: LinkPrecedence): Promise<Contact> => {
     const contact = new Contact();
     contact.email = email;
     contact.phoneNumber = phoneNumber;
-    contact.linkedId = null;
-    contact.linkPrecedence = LinkPrecedence.PRIMARY;
+    contact.linkedId = linkedId;
+    contact.linkPrecedence = linkPrecedence;
     return await contactRepository.save(contact);
 }
 
+const getContact = async (email?: string, phoneNumber?: string): Promise<Contact | null> => {
+    return await contactRepository.findOne({ 
+        where: {
+            email: email, 
+            phoneNumber: phoneNumber
+        } 
+    });
+};
+
+const getLinkedContacts = async (email: string, phoneNumber: string): Promise <Contact[]> => {
+    return await contactRepository.createQueryBuilder('contact')
+        .where('contact.email = :email OR contact.phoneNumber = :phoneNumber', { email, phoneNumber })
+        .orderBy('contact.createdAt', 'DESC')
+        .getMany();
+}
+
 export default {
-    createContact
+    createContact,
+    getContact,
+    getLinkedContacts
 };
---
+++ src/types/response/ContactResponses.ts
@@ -0,0 +1,8 @@
+export interface CreateContactResponse {
+    contact: {
+        primaryContactId: number; 
+        emails: string[]; // first element being email of primary contact 
+        phoneNumbers: string[]; // first element being phoneNumber of primary contact
+        secondaryContactIds: number[]; // Array of all Contact IDs that are "secondary" to the primary contact
+    };
+}
