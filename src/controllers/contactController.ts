e38f1f86ae614f60b0fe520f75ed88b1781320d4
meetakbari <meetakbari25@gmail.com>
2024-05-28 00:47:03 +0530

    Add: Contact Service to identify if contact already exists and create primary or secondary contact. Integration with contact controller.

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -1,17 +1,14 @@
 import { Request, Response } from "express-serve-static-core";
 import { createContact } from "../services/contactService";
+import { CreateContactResponse } from "../types/response/ContactResponses";
  
 export const createContactHandler = async (req: Request, res: Response) => {
   const { email, phoneNumber } = req.body;
 
   try {
-    const newContact = await createContact(email, phoneNumber);
-    res.status(201).json(newContact);
+    const response: CreateContactResponse = await createContact(email, phoneNumber);
+    res.status(201).json(response);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
-// export default {
-//     createContactHandler
-// };
--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -1,6 +1,43 @@
 import ContactRepository from "../repositories/ContactRepository";
-import { Contact } from "../db/models/Contact";
+import { Contact, LinkPrecedence } from "../db/models/Contact";
+import { CreateContactResponse } from "src/types/response/ContactResponses";
 
-export const createContact = async (email: string, phoneNumber: string): Promise<Contact> => {
-  return await ContactRepository.createContact(email, phoneNumber);
+export const createContact = async (email: string, phoneNumber: string): Promise<CreateContactResponse> => {
+  const linkedContacts: Contact[] = await ContactRepository.getLinkedContacts(email, phoneNumber);
+  
+  // check if contact with same email and phoneNumber already exists
+  const existingContact = await ContactRepository.getContact(email, phoneNumber);
+
+  if (!existingContact) {
+    // create a new contact
+    let linkedId: number = null;
+    let linkPrecedence: LinkPrecedence = LinkPrecedence.PRIMARY;
+
+    if (linkedContacts.length > 0) {
+      // get the id of the primary contact requires to pass in creation of secondary contact
+      const primaryContact = linkedContacts.find(contact => contact.linkPrecedence === 'primary');
+      linkedId = primaryContact.id;
+      linkPrecedence = LinkPrecedence.SECONDARY;
+    }
+
+    const newContact = await ContactRepository.createContact(email, phoneNumber, linkedId, linkPrecedence);
+
+    linkedContacts.unshift(newContact);
+  }
+  
+  const primaryContact = linkedContacts.find(contact => contact.linkPrecedence === 'primary');
+  const secondaryContacts = linkedContacts.filter(contact => contact.linkPrecedence === 'secondary');
+
+  const emails = Array.from(new Set([primaryContact.email, ...secondaryContacts.map(contact => contact.email)].filter(email => email !== null)));
+  const phoneNumbers = Array.from(new Set([primaryContact.phoneNumber, ...secondaryContacts.map(contact => contact.phoneNumber)].filter(phoneNumber => phoneNumber !== null)));
+  const secondaryContactIds = secondaryContacts.map(contact => contact.id);
+
+  return {
+    contact: {
+      primaryContactId: primaryContact.id,
+      emails,
+      phoneNumbers,
+      secondaryContactIds
+    }
+  };
 };
